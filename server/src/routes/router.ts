28a40e61184f63c3e5408926f1a488dab168a737
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-10-26 15:36:31 +0800

    Add new routes

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { default as pingRouter } from "./ping.ts";
 import { default as accountRouter } from "./account.ts";
 import { default as careerRouter } from "./career.ts";
+import { default as examRouter } from "./exam.ts";
 
 const router = express.Router();
 
 router.use("/ping", pingRouter);
 router.use("/account", accountRouter);
 router.use("/career", careerRouter);
+router.use("/exam", examRouter);
 
 export default router;
